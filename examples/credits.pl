54fd608adc50c46014a416bcaeacf7eaf90abdc7
Toby Inkster <mail@tobyinkster.co.uk>
2013-08-12 17:56:43 +0100

    create RDF::DOAP class abstracting the parsing of DOAP files into models

--- examples/credits.pl
+++ examples/credits.pl
@@ -4,7 +4,7 @@ use feature 'say';
 
 use RDF::Trine;
 use RDF::TrineX::Functions 'parse';
-use RDF::DOAP::Project;
+use RDF::DOAP;
 
 my $model = parse(
 	'http://api.metacpan.org/source/TOBYINK/MooX-ClassAttribute-0.008/META.ttl',
@@ -12,10 +12,7 @@ my $model = parse(
 	base => 'http://api.metacpan.org/source/TOBYINK/MooX-ClassAttribute-0.008/META.ttl',
 );
 
-my $obj = 'RDF::DOAP::Project'->rdf_load(
-	'http://purl.org/NET/cpan-uri/dist/MooX-ClassAttribute/project',
-	$model,
-);
+my $obj = 'RDF::DOAP'->from_model($model)->project;
 
 say "==== MAINTAINERS ====";
 say $_->to_string for $obj->gather_all_maintainers;
---
+++ lib/RDF/DOAP.pm
@@ -0,0 +1,89 @@
+package RDF::DOAP;
+
+our $AUTHORITY = 'cpan:TOBYINK';
+our $VERSION   = 0.001;
+
+use Moose;
+
+use RDF::DOAP::Types -types;
+use RDF::DOAP::Resource ();
+use RDF::DOAP::Project ();
+use RDF::DOAP::Repository ();
+use RDF::DOAP::Person ();
+use RDF::DOAP::Version ();
+use RDF::DOAP::ChangeSet ();
+use RDF::DOAP::Change ();
+use RDF::DOAP::Issue ();
+
+use RDF::Trine::Namespace qw(rdf rdfs owl xsd);
+my $doap = 'RDF::Trine::Namespace'->new('http://usefulinc.com/ns/doap#');
+
+has projects => (
+	is         => 'ro',
+	isa        => ArrayRef[Project],
+	default    => sub { [] },
+	coerce     => 1,
+);
+
+sub from_uri
+{
+	require RDF::Trine;
+	
+	my $class = shift;
+	my ($url) = @_;
+	
+	my $model = 'RDF::Trine::Model'->new;
+	'RDF::Trine::Parser'->parse_url_into_model($url, $model);
+	
+	return $class->from_model($model);
+}
+
+sub from_file
+{
+	require RDF::Trine;
+	
+	my $class = shift;
+	my ($fh, $base) = @_;
+	$base //= 'http://localhost/';
+	
+	my $model = 'RDF::Trine::Model'->new;
+	'RDF::Trine::Parser'->parse_file_into_model($base, $fh, $model);
+	
+	return $class->from_model($model);
+}
+
+sub from_model
+{
+	my $class = shift;
+	my ($model) = @_;
+	
+	# required for coercion to work!
+	local $RDF::DOAP::Resource::MODEL = $model;
+	
+	$class->new(
+		projects => [ $model->subjects($rdf->type, $doap->Project) ],
+	);
+}
+
+sub project
+{
+	my $self = shift;
+	
+	my @projects = @{$self->projects};
+	return $projects[0] if @projects <= 1;
+	
+	my @sorted = 
+		map $_->[0],
+		sort { $b->[1] <=> $a->[1] }
+		map [
+			$_,
+			$_->has_rdf_model && $_->has_rdf_about
+				? $_->rdf_model->count_statements($_->rdf_about, undef, undef)
+				: 0
+		], @projects;
+	
+	return $sorted[0];
+}
+
+1;
+
